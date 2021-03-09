0a17ec560d4168e8c23ca0d31e4c322360179a17
agis <agis.septiyanto@gmail.com>
2021-03-09 14:23:34 +0700

    bismillah mari masak

---
+++ app/src/main/java/id/agis/sakmasak/MainActivity.kt
@@ -0,0 +1,13 @@
+package id.agis.sakmasak
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.agis.sakmasak.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
