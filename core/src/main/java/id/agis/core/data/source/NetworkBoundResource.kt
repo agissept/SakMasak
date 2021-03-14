a186e468bb942fdf2563806411b36589a83f035e
agis <agista@dicoding.com>
2021-03-14 18:58:30 +0700

    create network bound resource

---
+++ core/src/main/java/id/agis/core/data/source/NetworkBoundResource.kt
@@ -0,0 +1,32 @@
+package id.agis.core.data.source
+
+import id.agis.core.data.source.remote.network.ApiResponse
+import kotlinx.coroutines.flow.*
+
+abstract class NetworkBoundResource<ResultType, RequestType> {
+
+    private var result: Flow<Resource<ResultType>> = flow {
+        emit(Resource.Loading())
+        when (val apiResponse = getDataFromNetwork().first()) {
+            is ApiResponse.Success -> {
+                emit(Resource.Success(convertDataToDomainModel(apiResponse.data)))
+            }
+            is ApiResponse.Empty -> {
+                emit(Resource.Error<ResultType>("Data is empty"))
+            }
+            is ApiResponse.Error -> {
+                onFetchFailed()
+                emit(Resource.Error<ResultType>(apiResponse.errorMessage))
+            }
+        }
+    }
+
+
+    protected open fun onFetchFailed() {}
+
+    protected abstract fun convertDataToDomainModel(data: RequestType): ResultType
+
+    protected abstract suspend fun getDataFromNetwork(): Flow<ApiResponse<RequestType>>
+
+    fun asFlow(): Flow<Resource<ResultType>> = result
+}
