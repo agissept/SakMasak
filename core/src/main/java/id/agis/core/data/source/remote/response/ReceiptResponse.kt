d92ab288c6aeb584d4b1a925609ed29d3a6cb210
agis <agista@dicoding.com>
2021-03-14 18:57:46 +0700

    create receipt response

---
+++ core/src/main/java/id/agis/core/data/source/remote/response/ReceiptResponse.kt
@@ -0,0 +1,23 @@
+package id.agis.core.data.source.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class ReceiptResponse(
+    @field:SerializedName("title")
+    val title: String,
+
+    @field:SerializedName("thumb")
+    val thumb: String,
+
+    @field:SerializedName("key")
+    val key: String,
+
+    @field:SerializedName("times")
+    val times: String,
+
+    @field:SerializedName("portion")
+    val portion: String,
+
+    @field:SerializedName("dificulty")
+    val difficulty: String
+)
