295397594e21678d03faef8d6f1f6f2445b5379d
agis <agis.septiyanto@gmail.com>
2021-03-16 10:02:50 +0700

    create initial DetailActivity

---
+++ app/src/main/java/id/agis/sakmasak/ui/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package id.agis.sakmasak.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.agis.sakmasak.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
