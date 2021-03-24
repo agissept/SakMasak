b5f473ae04b58e538beddebb13e1ea24a7ef4b75
agis <agis.septiyanto@gmail.com>
2021-03-24 15:22:16 +0700

    add favorite function in viewmodel

--- app/src/main/java/id/agis/sakmasak/ui/detail/DetailViewModel.kt
+++ app/src/main/java/id/agis/sakmasak/ui/detail/DetailViewModel.kt
@@ -5,10 +5,23 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import id.agis.core.data.source.Resource
 import id.agis.core.domain.model.DetailRecipe
+import id.agis.core.domain.model.RecipeItem
 import id.agis.core.domain.usecase.RecipeUseCase
 
 class DetailViewModel(private val useCase: RecipeUseCase) : ViewModel() {
     fun getGetDetailRecipe(recipeKey: String): LiveData<Resource<DetailRecipe>> {
         return useCase.getDetailRecipe(recipeKey).asLiveData()
+    }
+
+    fun addRecipeToFavorite(recipe: RecipeItem){
+        useCase.addRecipeToFavorite(recipe)
+    }
+
+    fun getFavoriteRecipeById(recipeKey: String): RecipeItem? {
+       return useCase.getFavoriteRecipeById(recipeKey)
+    }
+
+    fun removeRecipeFromFavorite(recipe: RecipeItem){
+        useCase.removeRecipeFromFavorite(recipe)
     }
 }
