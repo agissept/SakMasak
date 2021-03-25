4a6ac6a1aa84fad1814744d4d01f274e8acef98d
agis <agista@dicoding.com>
2021-03-25 15:52:49 +0700

    modify adapter to support paging

--- app/src/main/java/id/agis/sakmasak/ui/home/HomeAdapter.kt
+++ app/src/main/java/id/agis/sakmasak/ui/home/HomeAdapter.kt
@@ -3,6 +3,8 @@ package id.agis.sakmasak.ui.home
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import id.agis.core.domain.model.RecipeItem
@@ -10,42 +12,49 @@ import id.agis.sakmasak.databinding.ItemRecipeBinding
 import id.agis.sakmasak.ui.detail.DetailActivity
 import id.agis.sakmasak.ui.detail.DetailActivity.Companion.EXTRA_RECIPE_KEY
 
-class HomeAdapter : RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
-    private val listRecipe = mutableListOf<RecipeItem>()
-
-    fun setItem(listRecipe: List<RecipeItem>) {
-        this.listRecipe.addAll(listRecipe)
-        notifyDataSetChanged()
-    }
+class HomeAdapter : PagingDataAdapter<RecipeItem, HomeAdapter.ViewHolder>(RECIPE_COMPARATOR) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = ItemRecipeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val recipe = listRecipe[position]
-        with(holder.binding) {
-            with(recipe) {
-                tvTitle.text = title
-                tvDifficulty.text = difficulty
-                tvPortion.text = portion
-                tvTimes.text = times
-                ivThumb.load(thumb)
-            }
-        }
-
-        holder.itemView.setOnClickListener {
-            with(holder.itemView.context) {
-                val intent = Intent(this, DetailActivity::class.java).apply {
-                    putExtra(EXTRA_RECIPE_KEY, recipe.key)
+    override fun onBindViewHolder(holder: HomeAdapter.ViewHolder, position: Int) {
+        val recipe = getItem(position)
+        if (recipe != null) {
+            with(holder.binding) {
+                with(recipe) {
+                    tvTitle.text = title
+                    tvDifficulty.text = difficulty
+                    tvPortion.text = portion
+                    tvTimes.text = times
+                    ivThumb.load(thumb)
                 }
-                startActivity(intent)
+            }
+
+            holder.itemView.setOnClickListener {
+                with(holder.itemView.context) {
+                    val intent = Intent(this, DetailActivity::class.java).apply {
+                        putExtra(EXTRA_RECIPE_KEY, recipe.key)
+                    }
+                    startActivity(intent)
+                }
             }
         }
     }
 
-    override fun getItemCount() = listRecipe.size
-
     inner class ViewHolder(val binding: ItemRecipeBinding) : RecyclerView.ViewHolder(binding.root)
+
+    companion object {
+        private val RECIPE_COMPARATOR = object : DiffUtil.ItemCallback<RecipeItem>() {
+            override fun areItemsTheSame(oldItem: RecipeItem, newItem: RecipeItem): Boolean {
+                return true
+            }
+
+            override fun areContentsTheSame(oldItem: RecipeItem, newItem: RecipeItem): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
 }
