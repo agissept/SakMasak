ef7b256de1194b1319fd770d2caed358dd36257a
agis <agista@dicoding.com>
2021-03-25 15:53:12 +0700

    modify activity to support paging

--- app/src/main/java/id/agis/sakmasak/ui/home/HomeActivity.kt
+++ app/src/main/java/id/agis/sakmasak/ui/home/HomeActivity.kt
@@ -2,11 +2,12 @@ package id.agis.sakmasak.ui.home
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import id.agis.core.data.source.Resource
 import id.agis.sakmasak.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import org.koin.android.viewmodel.ext.android.viewModel
 
 
@@ -15,7 +16,7 @@ class HomeActivity : AppCompatActivity() {
     private val viewModel: HomeViewModel by viewModel()
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var adapter: HomeAdapter
+    private val recipeAdapter = HomeAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,27 +28,17 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun observeListRecipe() {
-        viewModel.listRecipe.observe(this, {
-            if (it.data != null) {
-                when (it) {
-                    is Resource.Loading -> {
-                        binding.progressCircular.visibility = View.VISIBLE
-                    }
-                    is Resource.Success -> {
-                        adapter.setItem(it.data!!)
-                        binding.progressCircular.visibility = View.GONE
-                    }
-                    is Resource.Error -> {
-                        Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
-                    }
-                }
+        // TODO fix loading
+        binding.progressCircular.visibility = View.GONE
+        lifecycleScope.launch {
+            viewModel.listRecipe.collectLatest {
+                recipeAdapter.submitData(it)
             }
-        })
+        }
     }
 
     private fun initRecyclerView() {
-        adapter = HomeAdapter()
-        binding.recyclerView.adapter = adapter
+        binding.recyclerView.adapter = recipeAdapter
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
     }
 
