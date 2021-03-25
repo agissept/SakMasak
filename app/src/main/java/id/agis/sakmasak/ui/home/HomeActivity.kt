770356cb29178a94512a372b7d32d99c622337cf
agis <agista@dicoding.com>
2021-03-25 16:25:07 +0700

    add loader while load recipes

--- app/src/main/java/id/agis/sakmasak/ui/home/HomeActivity.kt
+++ app/src/main/java/id/agis/sakmasak/ui/home/HomeActivity.kt
@@ -1,11 +1,14 @@
 package id.agis.sakmasak.ui.home
 
 import android.os.Bundle
-import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
 import id.agis.sakmasak.databinding.ActivityMainBinding
+import id.agis.sakmasak.ui.home.loader.HomeLoadStateAdapter
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -16,29 +19,49 @@ class HomeActivity : AppCompatActivity() {
     private val viewModel: HomeViewModel by viewModel()
 
     private lateinit var binding: ActivityMainBinding
-    private val recipeAdapter = HomeAdapter()
+    private val adapter = HomeAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initRecyclerView()
+        initRecyclerViewAdapter()
         observeListRecipe()
     }
 
     private fun observeListRecipe() {
         // TODO fix loading
-        binding.progressCircular.visibility = View.GONE
+//        binding.progressCircular.visibility = View.GONE
         lifecycleScope.launch {
             viewModel.listRecipe.collectLatest {
-                recipeAdapter.submitData(it)
+                adapter.submitData(it)
             }
         }
     }
 
-    private fun initRecyclerView() {
-        binding.recyclerView.adapter = recipeAdapter
+    private fun initRecyclerViewAdapter() {
+        binding.recyclerView.adapter =
+            adapter.withLoadStateHeaderAndFooter(
+                header = HomeLoadStateAdapter { adapter.retry() },
+                footer = HomeLoadStateAdapter { adapter.retry() }
+            )
+        adapter.addLoadStateListener { loadState ->
+            binding.recyclerView.isVisible = loadState.source.refresh is LoadState.NotLoading
+            binding.progressCircular.isVisible = loadState.source.refresh is LoadState.Loading
+            binding.retryButton.isVisible = loadState.source.refresh is LoadState.Error
+
+            val errorState = loadState.source.append as? LoadState.Error
+                ?: loadState.source.prepend as? LoadState.Error
+                ?: loadState.append as? LoadState.Error
+                ?: loadState.prepend as? LoadState.Error
+
+            errorState?.let {
+                Toast.makeText(
+                    this, "\uD83D\uDE28 Wooops ${it.error}", Toast.LENGTH_LONG
+                ).show()
+            }
+        }
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
     }
 
---
+++ app/src/main/java/id/agis/sakmasak/ui/home/loader/HomeLoadStateAdapter.kt
@@ -0,0 +1,15 @@
+package id.agis.sakmasak.ui.home.loader
+
+import android.view.ViewGroup
+import androidx.paging.LoadState
+import androidx.paging.LoadStateAdapter
+
+class HomeLoadStateAdapter(private val retry: () -> Unit) : LoadStateAdapter<HomeLoadStateViewHolder>() {
+    override fun onBindViewHolder(holder: HomeLoadStateViewHolder, loadState: LoadState) {
+        holder.bind(loadState)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, loadState: LoadState): HomeLoadStateViewHolder {
+        return HomeLoadStateViewHolder.create(parent, retry)
+    }
+}
