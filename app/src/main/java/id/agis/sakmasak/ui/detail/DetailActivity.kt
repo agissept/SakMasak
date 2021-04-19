f935c286401742f6340cc6e58ddc93a6b1d41de9
agis <agis.septiyanto@gmail.com>
2021-04-19 22:54:04 +0700

    remove toast

--- app/src/main/java/id/agis/sakmasak/ui/detail/DetailActivity.kt
+++ app/src/main/java/id/agis/sakmasak/ui/detail/DetailActivity.kt
@@ -57,10 +57,8 @@ class DetailActivity : AppCompatActivity() {
             val scrollY: Int = binding.mainView.scrollY
             if (scrollY > 900 && window.statusBarColor != Color.BLACK) {
                 window.statusBarColor = Color.BLACK
-                Toast.makeText(this, "black", Toast.LENGTH_SHORT).show()
             }else if(scrollY < 900 && window.statusBarColor != Color.TRANSPARENT){
                 window.statusBarColor = Color.TRANSPARENT
-                Toast.makeText(this, "white", Toast.LENGTH_SHORT).show()
             }
         }
     }
